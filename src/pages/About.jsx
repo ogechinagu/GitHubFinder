6450ded082dcf7d0805194a413020fb8e2d8703c
ogechinagu <mzogechee@gmail.com>
2022-11-04 10:31:37 +0100

    Added env variables

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details created by
+        <a href='https://twitter.com/ogechinagu'>
+          {' '}
+          <strong>Ogechi Ikwunagu</strong>
+        </a>{' '}
+        following the tutorials{' '}
+        <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
+          React Front To Back
+        </a>{' '}
+        Udemy course by{' '}
+        <strong>
+          <a href='https://traversymedia.com'> Brad Traversy</a>
+        </strong>
+        .
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+      <p className='text-lg text-gray-400'>
+        Layout By:
+        <a className='text-white' href='https://twitter.com/hassibmoddasser'>
+          Hassib Moddasser
+        </a>
+      </p>
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <h1 className='text-6xl'>Welcome</h1>
+    </>
+  );
+};
