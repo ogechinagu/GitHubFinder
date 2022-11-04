4cd6f7de8cce1442d02340cca871fd555b731ff5
ogechinagu <mzogechee@gmail.com>
2022-11-04 09:59:27 +0100

    Created Nav, Footer and Pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Footer } from './components/layouts/Footer';
+import { Navbar } from './components/layouts/Navbar';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
